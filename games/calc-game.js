7b3109bf621ba82eb175d104abca7758d56555db
Natalia Nikitina <nikitina.n.84@yandex.ru>
2021-12-11 18:43:56 +0500

    edit calc-game.js

--- games/calc-game.js
+++ games/calc-game.js
@@ -1,5 +1,6 @@
 import startGame from '../scr/index.js';
 import getRandomNumber from '../scr/getRandomNumber.js';
+import getRandomElement from '../scr/getRandomElement.js';
 
 const gameQuestion = 'What is the result of the expression?';
 
@@ -22,7 +23,7 @@ const calculateResult = (operator, num1, num2) => {
 const gameInfo = () => {
   const num1 = getRandomNumber(1, 30);
   const num2 = getRandomNumber(1, 30);
-  const operator = operators[Math.floor(Math.random() * operators.length)];
+  const operator = getRandomElement(operators);
   const question = `${num1} ${operator} ${num2}`;
   const answer = String(calculateResult(operator, num1, num2));
   return [question, answer];
