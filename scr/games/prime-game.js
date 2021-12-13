69576cb46262f8c4c1bce6797f63c330a0297bb0
Natalia Nikitina <nikitina.n.84@yandex.ru>
2021-12-14 01:15:31 +0500

    edit prime-game.js

--- scr/games/prime-game.js
+++ scr/games/prime-game.js
@@ -4,6 +4,9 @@ import getRandomNumber from '../helpers/getRandomNumber.js';
 const gameQuestion = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
   let divider = 2;
   while (divider <= number / 2) {
     if (number % divider === 0) {
