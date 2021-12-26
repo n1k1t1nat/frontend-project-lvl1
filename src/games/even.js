8452d2ec3a6de3bf6219576162a1473e715f2210
Natalia Nikitina <nikitina.n.84@yandex.ru>
2021-12-26 23:24:42 +0500

    update even.js and prime.js

--- src/games/even.js
+++ src/games/even.js
@@ -3,21 +3,18 @@ import getRandomNumber from '../helpers/getRandomNumber.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = () => {
+const isEven = (number) => (number % 2 === 0);
+
+const makeRound = () => {
   const question = getRandomNumber(1, 100);
-  let answer;
-  if (question % 2 === 0) {
-    answer = 'yes';
-  } else {
-    answer = 'no';
-  }
+  const answer = isEven(question) ? 'yes' : 'no';
   return [question, answer];
 };
 
 export default () => {
   const gameRounds = [];
   for (let i = 1; gameRounds.length < roundsCount; i += 1) {
-    gameRounds.push(isEven());
+    gameRounds.push(makeRound());
   }
   playGame(description, gameRounds);
 };
--- src/games/prime.js
+++ src/games/prime.js
@@ -3,24 +3,30 @@ import getRandomNumber from '../helpers/getRandomNumber.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const isPrime = () => {
-  const question = getRandomNumber(1, 100);
-  let answer = 'yes';
-  if (question < 2) {
-    answer = 'no';
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
   }
-  for (let divider = 2; divider <= question / 2; divider += 1) {
-    if (question % divider === 0) {
-      answer = 'no';
+  let divider = 2;
+  while (divider <= number / 2) {
+    if (number % divider === 0) {
+      return false;
     }
+    divider += 1;
   }
+  return true;
+};
+
+const makeRound = () => {
+  const question = getRandomNumber(1, 100);
+  const answer = isPrime(question) ? 'yes' : 'no';
   return [question, answer];
 };
 
 export default () => {
   const gameRounds = [];
   for (let i = 1; gameRounds.length < roundsCount; i += 1) {
-    gameRounds.push(isPrime());
+    gameRounds.push(makeRound());
   }
   playGame(description, gameRounds);
 };
